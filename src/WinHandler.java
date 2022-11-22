b5ec3deaae7698752058c561180c60c9e6029008
Sam Barker <sjb296@exeter.ac.uk>
2022-11-22 16:01:02 +0000

    Commented WinHandler

--- src/WinHandler.java
+++ src/WinHandler.java
@@ -1,3 +1,8 @@
+/**
+ * Objects of this class hold the number of the winning player (initially -1
+ * because nobody has won yet) and are asked it by Players so that they
+ * can tell if any other Player has won the game.
+ */
 public class WinHandler {
     private int winner = -1;
 
