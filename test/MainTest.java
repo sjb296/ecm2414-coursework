b67536f64fc1debd400e12ceb9457f0cbeeed581
Sam Barker <sjb296@exeter.ac.uk>
2022-11-22 16:16:29 +0000

    Deleted unnecessary MainTest file

--- test/MainTest.java
+++
@@ -1,8 +0,0 @@
-import org.junit.runner.JUnitCore;
-
-public class MainTest {
-    // Will run all Test Suites
-    public static void main(String[] args) {
-        JUnitCore.runClasses(TestPrimer.class);
-    }
-}
